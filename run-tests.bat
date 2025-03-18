cedb051ef0446d5c1f20cbaffbe616b0d2db03d8
SestrenExsis <sestrenexsis@gmail.com>
2025-03-17 22:27:19 -0500

    Verify another PPF

--- run-tests.bat
+++ run-tests.bat
@@ -1,7 +1,10 @@
 
 python src/sotn_extractor.py "build/Castlevania - Symphony of the Night (Track 1).bin" "build/extraction.json" || goto :error
 python src/sotn_patcher.py "build/extraction.json" --changes="tests/softlock-in-alchemy-lab-maria-cutscene.json" --aliases="data/aliases.yaml" --ppf="build/ppf/softlock-in-alchemy-lab-maria-cutscene.ppf" || goto :error
+python src/sotn_patcher.py "build/extraction.json" --changes="tests/customized-castle-map-reveals.json" --aliases="data/aliases.yaml" --ppf="build/ppf/customized-castle-map-reveals.ppf" || goto :error
+
 sha1sum -c tests/softlock-in-alchemy-lab-maria-cutscene.sha1
+sha1sum -c tests/customized-castle-map-reveals.sha1
 
 goto :EOF
 
