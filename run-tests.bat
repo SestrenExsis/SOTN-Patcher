919f1cb82daa3a0e997a85d8fae48c61825da32a
SestrenExsis <sestrenexsis@gmail.com>
2025-03-18 21:16:02 -0500

    Validate old method of patching familiar events

--- run-tests.bat
+++ run-tests.bat
@@ -2,9 +2,9 @@
 python src/sotn_extractor.py "build/Castlevania - Symphony of the Night (Track 1).bin" "build/extraction.json" || goto :error
 python src/sotn_patcher.py "build/extraction.json" --changes="tests/softlock-in-alchemy-lab-maria-cutscene.json" --aliases="data/aliases.yaml" --ppf="build/ppf/softlock-in-alchemy-lab-maria-cutscene.ppf" || goto :error
 python src/sotn_patcher.py "build/extraction.json" --changes="tests/customized-castle-map-reveals.json" --aliases="data/aliases.yaml" --ppf="build/ppf/customized-castle-map-reveals.ppf" || goto :error
+python src/sotn_patcher.py "build/extraction.json" --changes="tests/sample-randomized-map.json" --aliases="data/aliases.yaml" --ppf="build/ppf/sample-randomized-map.ppf" || goto :error
 
-sha1sum -c tests/softlock-in-alchemy-lab-maria-cutscene.sha1
-sha1sum -c tests/customized-castle-map-reveals.sha1
+sha1sum -c tests/checksums.sha1
 
 goto :EOF
 
