f26518c03ddaa7578d96650f4e61a21be5c65a5f
SestrenExsis <sestrenexsis@gmail.com>
2025-05-03 07:31:38 -0500

    Access Normalize room connections through Options

--- src/sotn_patcher.py
+++ src/sotn_patcher.py
@@ -327,7 +327,7 @@ def get_ppf(extract, changes, data):
                 value = a_value if type == 'A' else b_value
                 result.patch_value(value, 'u32', sotn_address.Address(base + offset))
     # Room shuffler - Normalize room connections
-    if changes.get('Normalize room connections', False):
+    if changes.get('Options', {}).get('Normalize room connections', False):
         for (offset, value) in (
             # Shift the breakable floor in Underground Caverns to the right 3 tiles
             # https://github.com/SestrenExsis/SOTN-Shuffler/issues/82
